85ed56350479977084b0128843052cbdaecef21d
diegomuuniz <diegomuuniz@outlook.com>
2020-02-04 11:14:40 -0300

    Config and User class adjustments

--- app/src/test/java/com/example/da_app_test_v1101/Config.java
+++ app/src/test/java/com/example/da_app_test_v1101/Config.java
@@ -1,4 +1,37 @@
 package com.example.da_app_test_v1101;
 
+/**
+ * OCARIoT DATA ACQUISITION APP
+ * version: v1.10.1
+ * APK: https://github.com/ocariot/da-app/releases
+ * Dashboard: https://ocariot-nutes-dashboard.firebaseapp.com/
+ * https://www.ocariot.com/
+ * https://www.ocariot.com.br/
+ *
+ * Tester: DIEGO MUNIZ
+ * Graduanting Computer Science, UEPB.
+ *
+ * APPIUM SETTINGS
+ *
+ * */
+
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 public class Config {
+    /* APPIUM Configuration */
+
+    public static String url = "http://localhost:4723/wd/hub";
+
+    public static void configAppium(DesiredCapabilities config) {
+        /* Android emulator or device */
+        config.setCapability("deviceName", "5200f3bbecc1b4db");
+        /* Test platform */
+        config.setCapability("platformName", "Android");
+        /* Windows */
+//        config.setCapability("app", "C:\\Users\\diego\\Downloads\\ocariot_da_1.9.4_debug.apk");
+        /* Linux */
+        config.setCapability("app", "/home/diego/Downloads/ocariot_da_v1.9.5_debug.apk");
+        /*URL remoteUrl = new URL(url);*/
+    }
 }
---
+++ app/src/test/java/com/example/da_app_test_v1101/User.java
@@ -0,0 +1,41 @@
+package com.example.da_app_test_v1101;
+
+/**
+ * OCARIoT DATA ACQUISITION APP
+ * version: v1.10.1
+ * APK: https://github.com/ocariot/da-app/releases
+ * Dashboard: https://ocariot-nutes-dashboard.firebaseapp.com/
+ * https://www.ocariot.com/
+ * https://www.ocariot.com.br/
+ *
+ * Tester: DIEGO MUNIZ
+ * Graduanting Computer Science, UEPB.
+ *
+ * List ID:
+ * username: br.edu.uepb.nutes.ocariot:id/username
+ * password: br.edu.uepb.nutes.ocariot:id/password
+ * login button: br.edu.uepb.nutes.ocariot:id/sign_in_button
+ *
+ * alert title: br.edu.uepb.nutes.ocariot:id/tvTitle
+ * */
+
+import org.junit.Assert;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class User {
+
+    public void login(AndroidDriver driver, String username, String password) {
+        /* username */
+        driver.findElementById("br.edu.uepb.nutes.ocariot:id/username").sendKeys(username);
+        /* password */
+        driver.findElementById("br.edu.uepb.nutes.ocariot:id/password").sendKeys(password);
+        /* login button */
+        driver.findElementById("br.edu.uepb.nutes.ocariot:id/sign_in_button").click();
+    }
+
+    public static void authenticationFailed(AndroidDriver driver) {
+        Assert.assertEquals("Authentication failed!", driver.findElementById("br.edu.uepb.nutes.ocariot:id/tvTitle").getText());
+    }
+
+}
