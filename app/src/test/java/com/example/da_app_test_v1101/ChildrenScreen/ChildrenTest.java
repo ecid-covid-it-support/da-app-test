c698e29df63a6eb6bf5c8b001c59479c7afede2e
diegomuuniz <diegomuuniz@outlook.com>
2020-02-04 12:33:16 -0300

    added package ChildrenScreen and Children class

---
+++ app/src/test/java/com/example/da_app_test_v1101/ChildrenScreen/ChildrenTest.java
@@ -0,0 +1,23 @@
+package com.example.da_app_test_v1101.ChildrenScreen;
+
+/**
+ * OCARIoT DATA ACQUISITION APP
+ * version: v1.10.1
+ * APK: https://github.com/ocariot/da-app/releases
+ * Dashboard: https://ocariot-nutes-dashboard.firebaseapp.com/
+ * https://www.ocariot.com/
+ * https://www.ocariot.com.br/
+ *
+ * Tester: DIEGO MUNIZ
+ * Graduanting Computer Science, UEPB.
+ *
+ * Screen: Children
+ * id screen: SC2
+ *
+ * List ID:
+ *
+ */
+
+public class ChildrenTest {
+
+}
