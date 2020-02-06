52c15e611b5c2dd81e6dbef480a0a2727b614f68
diegomuuniz <diegomuuniz@outlook.com>
2020-02-06 11:10:35 -0300

    TC020 and TC021 passed

--- app/src/test/java/com/example/da_app_test_v1101/welcomeScreen/WelcomeTest.java
+++ app/src/test/java/com/example/da_app_test_v1101/welcomeScreen/WelcomeTest.java
@@ -18,6 +18,10 @@ package com.example.da_app_test_v1101.welcomeScreen;
  *
  * <p>
  * List ID:
+ * children button: br.edu.uepb.nutes.ocariot:id/action_child
+ * settings button: br.edu.uepb.nutes.ocariot:id/action_settings
+ * fitbit button: br.edu.uepb.nutes.ocariot:id/fitbit_button
+ * do not use fitbit: br.edu.uepb.nutes.ocariot:id/do_not_login_fitbit_button
  *
  * <p>
  * TEST CASES:
@@ -42,18 +46,23 @@ import com.example.da_app_test_v1101.Config;
 import com.example.da_app_test_v1101.User;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import okhttp3.MultipartBody;
 
 @RunWith(Parameterized.class)
 public class WelcomeTest {
@@ -98,19 +107,38 @@ public class WelcomeTest {
         });
     }
 
-    private void welcomeScreen() {
-
+    private void welcomeScreen() throws InterruptedException {
+        User.login(driver, this.validUsername, this.validPassword);
+        Thread.sleep(7000);
+        /* Children list */
+        MobileElement children_list = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/children_list");
+        /* List children */
+        List<MobileElement> children = children_list.findElements(By.className("android.widget.RelativeLayout"));
+        /* List status Fitbit */
+        children.get(1).click();
+        Thread.sleep(2000);
     }
 
     @Test
     /* TC020 */
-    public void childrenButton() {
-
+    public void childrenButton() throws InterruptedException {
+        welcomeScreen();
+        MobileElement children_button = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/action_child");
+        children_button.click();
+        Thread.sleep(2000);
+        MobileElement toolbar = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/toolbar");
+        Assert.assertEquals("Children", toolbar.findElement(By.className("android.widget.TextView")).getText());
     }
 
     @Test
     /* TC021 */
-    public void settingsButtons() {
+    public void settingsButtons() throws InterruptedException {
+        welcomeScreen();
+        MobileElement settings_button = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/action_settings");
+        settings_button.click();
+        Thread.sleep(4000);
+        MobileElement action_bar = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/action_bar");
+        Assert.assertEquals("Settings", action_bar.findElements(By.className("android.widget.TextView")).get(0).getText());
 
     }
 
