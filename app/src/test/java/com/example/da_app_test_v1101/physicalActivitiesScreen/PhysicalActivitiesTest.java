e4198d5d5d872bf752f63da33b34ddf2ac8ba151
diegomuuniz <diegomuuniz@outlook.com>
2020-02-06 13:06:19 -0300

    details activities is than zero - TC032

--- app/src/test/java/com/example/da_app_test_v1101/physicalActivitiesScreen/PhysicalActivitiesTest.java
+++ app/src/test/java/com/example/da_app_test_v1101/physicalActivitiesScreen/PhysicalActivitiesTest.java
@@ -15,12 +15,36 @@ package com.example.da_app_test_v1101.physicalActivitiesScreen;
  * id screen: SC3
  * <p>
  * List ID:
- * children button: br.edu.uepb.nutes.ocariot:id/action_child
- * settings button: br.edu.uepb.nutes.ocariot:id/action_settings
- * fitbit button: br.edu.uepb.nutes.ocariot:id/fitbit_button
- * do not use fitbit: br.edu.uepb.nutes.ocariot:id/do_not_login_fitbit_button
- * toolbar: br.edu.uepb.nutes.ocariot:id/toolbar
- * action bar: br.edu.uepb.nutes.ocariot:id/action_bar
+ * list activities: br.edu.uepb.nutes.ocariot:id/activities_list
+ * list duration: br.edu.uepb.nutes.ocariot:id/duration_tv
+ * list calories: br.edu.uepb.nutes.ocariot:id/calories_tv
+ * list distance: 	br.edu.uepb.nutes.ocariot:id/distance_tv
+ * icon activities: br.edu.uepb.nutes.ocariot:id/activity_img
+ * <p>
+ * clock img: br.edu.uepb.nutes.ocariot:id/clock_img
+ * details duration: br.edu.uepb.nutes.ocariot:id/activity_duration_tv
+ * details steps: br.edu.uepb.nutes.ocariot:id/activity_steps_tv
+ * details calories: br.edu.uepb.nutes.ocariot:id/activity_calories_tv
+ * details distance: br.edu.uepb.nutes.ocariot:id/activity_distance_tv
+ * details cal/min: br.edu.uepb.nutes.ocariot:id/activity_calories_min_tv
+ * <p>
+ * action box levels: br.edu.uepb.nutes.ocariot:id/activity_box_levels
+ * metrics box:
+ * br.edu.uepb.nutes.ocariot:id/box_metrics_1
+ * br.edu.uepb.nutes.ocariot:id/box_metrics_2
+ * <p>
+ * levels bar:
+ * br.edu.uepb.nutes.ocariot:id/sedentary_Level_tv
+ * br.edu.uepb.nutes.ocariot:id/lightly_Level_tv
+ * br.edu.uepb.nutes.ocariot:id/fairly_Level_tv
+ * br.edu.uepb.nutes.ocariot:id/very_Level_tv
+ * <p>
+ * levels value:
+ * br.edu.uepb.nutes.ocariot:id/sedentary_Level_value_tv
+ * br.edu.uepb.nutes.ocariot:id/lightly_Level_value_tv
+ * br.edu.uepb.nutes.ocariot:id/fairly_Level_value_tv
+ * br.edu.uepb.nutes.ocariot:id/very_Level_value_tv
+ *
  * <p>
  * TEST CASES:
  * TC032: Checks wheter the activity list data matches the details
@@ -37,6 +61,7 @@ import com.example.da_app_test_v1101.Config;
 import com.example.da_app_test_v1101.User;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -97,6 +122,18 @@ public class PhysicalActivitiesTest {
         });
     }
 
+    private void stringForNumber(String idElement) {
+        MobileElement text = (MobileElement) driver.findElementById(idElement);
+        String nbr = text.getText().replaceAll("[\\D]", "");
+        float number = Float.parseFloat(nbr);
+        Assert.assertTrue(number >= 0);
+    }
+
+    private void isgreaterThanZero(String textElement) {
+        float number = Float.parseFloat(textElement);
+        Assert.assertTrue(number >= 0);
+    }
+
     private void activityScreen() throws InterruptedException {
         User.login(driver, this.validUsername, this.validPassword);
         Thread.sleep(7000);
@@ -109,9 +146,36 @@ public class PhysicalActivitiesTest {
         Thread.sleep(2000);
     }
 
+
     @Test
     /* TC032 */
-    public void dataList() {
+    public void dataList() throws InterruptedException {
+        activityScreen();
+
+        MobileElement duration_list = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/duration_tv");
+        MobileElement calories_list = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/calories_tv");
+        MobileElement distance_list = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/distance_tv");
+        /* data list greater than zero */
+        isgreaterThanZero(duration_list.getText());
+        isgreaterThanZero(calories_list.getText());
+        isgreaterThanZero(distance_list.getText());
+
+        /* Activities list */
+        MobileElement activities_list = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/activities_list");
+        /* List activities */
+        List<MobileElement> activities = activities_list.findElements(By.className("android.widget.RelativeLayout"));
+        /* click fist activity */
+        activities.get(0).click();
+
+        Thread.sleep(2000);
+
+        stringForNumber("br.edu.uepb.nutes.ocariot:id/activity_duration_tv");
+
+        isgreaterThanZero(driver.findElementById("br.edu.uepb.nutes.ocariot:id/activity_steps_tv").getAttribute("text"));
+        isgreaterThanZero(driver.findElementById("br.edu.uepb.nutes.ocariot:id/activity_calories_tv").getAttribute("text"));
+        isgreaterThanZero(driver.findElementById("br.edu.uepb.nutes.ocariot:id/activity_distance_tv").getAttribute("text"));
+        isgreaterThanZero(driver.findElementById("br.edu.uepb.nutes.ocariot:id/activity_calories_min_tv").getAttribute("text"));
+
 
     }
 
