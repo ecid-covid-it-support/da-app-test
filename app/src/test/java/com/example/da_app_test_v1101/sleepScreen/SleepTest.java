a579c46639c80059793c2d8de7f4d6079b31f66c
diegomuuniz <diegomuuniz@outlook.com>
2020-02-13 12:03:42 -0300

    Adjustments sleep class

--- app/src/test/java/com/example/da_app_test_v1101/sleepScreen/SleepTest.java
+++ app/src/test/java/com/example/da_app_test_v1101/sleepScreen/SleepTest.java
@@ -1,4 +1,99 @@
 package com.example.da_app_test_v1101.sleepScreen;
 
+/**
+ * OCARIoT DATA ACQUISITION APP
+ * version: v1.10.1
+ * APK: https://github.com/ocariot/da-app/releases
+ * Dashboard: https://ocariot-nutes-dashboard.firebaseapp.com/
+ * https://www.ocariot.com/
+ * https://www.ocariot.com.br/
+ * <p>
+ * Tester: DIEGO MUNIZ
+ * Graduanting Computer Science, UEPB.
+ * <p>
+ * Screen: Sleep
+ * id screen: SC5
+ * <p>
+ * List ID:
+ * sleep list: br.edu.uepb.nutes.ocariot:id/sleep_list
+ * efficiency list: br.edu.uepb.nutes.ocariot:id/sleep_efficiency_tv
+ *
+ * sleep chat: br.edu.uepb.nutes.ocariot:id/sleep_chart
+ **/
+
+import com.example.da_app_test_v1101.BuildConfig;
+import com.example.da_app_test_v1101.Config;
+import com.example.da_app_test_v1101.User;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+@RunWith(Parameterized.class)
 public class SleepTest {
+
+    private AndroidDriver driver;
+    private DesiredCapabilities dc = new DesiredCapabilities();
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        Config.configAppium(dc);
+        URL remoteUrl = new URL(Config.url);
+        driver = new AndroidDriver(remoteUrl, dc);
+    }
+
+    private String validUsername;
+    private String validPassword;
+
+    User user;
+
+    public SleepTest (
+            String validUsername,
+            String validPassword
+    ) {
+        this.validUsername = validUsername;
+        this.validPassword = validPassword;
+    }
+
+    @Before
+    public void initialize() {
+        user = new User();
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                /*{
+                    1° valid username,
+                    2° valid password
+                  }*/
+                {BuildConfig.USERNAME_ED, BuildConfig.PASSWORD},
+                {BuildConfig.USERNAME_HP, BuildConfig.PASSWORD},
+                {BuildConfig.USERNAME_FM, BuildConfig.PASSWORD}
+        });
+    }
+
+    private void sleepScreen() throws InterruptedException {
+        User.login(driver, this.validUsername, this.validPassword);
+        Thread.sleep(7000);
+        /* Children list */
+        MobileElement children_list = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/children_list");
+        /* List children */
+        List<MobileElement> children = children_list.findElements(By.className("android.widget.RelativeLayout"));
+        /* List status Fitbit */
+        children.get(0).click();
+        Thread.sleep(2000);
+    }
+
 }
