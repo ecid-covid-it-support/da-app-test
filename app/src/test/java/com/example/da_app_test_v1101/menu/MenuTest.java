0ec8258caaefa2eab592d5db8d81f4e05e5265e5
diegomuuniz <diegomuuniz@outlook.com>
2020-02-13 15:58:07 -0300

    Created and updated: Tests of the MenuTest.java class

--- app/src/test/java/com/example/da_app_test_v1101/menu/MenuTest.java
+++ app/src/test/java/com/example/da_app_test_v1101/menu/MenuTest.java
@@ -3,18 +3,24 @@ package com.example.da_app_test_v1101.menu;
 import com.example.da_app_test_v1101.BuildConfig;
 import com.example.da_app_test_v1101.Config;
 import com.example.da_app_test_v1101.User;
+import com.google.common.io.LittleEndianDataInputStream;
 
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 
 /**
@@ -24,16 +30,25 @@ import io.appium.java_client.android.AndroidDriver;
  * Dashboard: https://ocariot-nutes-dashboard.firebaseapp.com/
  * https://www.ocariot.com/
  * https://www.ocariot.com.br/
- *
+ * <p>
  * Tester: DIEGO MUNIZ
  * Graduanting Computer Science, UEPB.
- *
+ * <p>
  * Screen: Physical Activities
  * id screen: SC4
  * <p>
  * List ID:
- *
- *
+ * <p>
+ * toolbar: br.edu.uepb.nutes.ocariot:id/toolbar - class: android.widget.TextView
+ * <p>
+ * children button: br.edu.uepb.nutes.ocariot:id/action_child
+ * settings button: br.edu.uepb.nutes.ocariot:id/action_settings
+ * menu bar: br.edu.uepb.nutes.ocariot:id/navigation
+ * physical activities: br.edu.uepb.nutes.ocariot:id/navigation_activities
+ * sleep: br.edu.uepb.nutes.ocariot:id/navigation_sleep
+ * IoT: br.edu.uepb.nutes.ocariot:id/navigation_iot
+ * <p>
+ * no data: br.edu.uepb.nutes.ocariot:id/box_no_data
  **/
 
 @RunWith(Parameterized.class)
@@ -54,7 +69,7 @@ public class MenuTest {
 
     User user;
 
-    public MenuTest (
+    public MenuTest(
             String validUsername,
             String validPassword
     ) {
@@ -81,27 +96,78 @@ public class MenuTest {
     }
 
 
+    private void menuScreen() throws InterruptedException {
+        User.login(driver, this.validUsername, this.validPassword);
+        Thread.sleep(7000);
+        /* Children list */
+        MobileElement children_list = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/children_list");
+        /* List children */
+        List<MobileElement> children = children_list.findElements(By.className("android.widget.RelativeLayout"));
+        /* List status Fitbit */
+        children.get(0).click();
+        Thread.sleep(2000);
+        MobileElement do_not_fitbit = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/do_not_login_fitbit_button");
+        do_not_fitbit.click();
+        Thread.sleep(2000);
+    }
+
     @Test
     /* TC065 */
-    public void childrenScreen() {
-
+    public void childrenScreen() throws InterruptedException {
+        menuScreen();
+        MobileElement children_button = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/action_child");
+        children_button.click();
+        Thread.sleep(2000);
+        MobileElement toolbar = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/toolbar");
+        Assert.assertEquals("Children", toolbar.findElement(By.className("android.widget.TextView")).getText());
     }
 
     @Test
     /* TC066 */
-    public void settingsScreen() {
-
+    public void settingsScreen() throws InterruptedException {
+        menuScreen();
+        MobileElement settings_button = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/action_settings");
+        settings_button.click();
+        Thread.sleep(4000);
+        MobileElement action_bar = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/action_bar");
+        Assert.assertEquals("Settings", action_bar.findElements(By.className("android.widget.TextView")).get(0).getText());
     }
 
     @Test
     /* TC067 */
-    public void correctScreen() {
-
+    public void correctScreenSleep() throws InterruptedException {
+        menuScreen();
+        MobileElement sleep = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/navigation_sleep");
+        MobileElement toolbar = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/toolbar");
+        List<MobileElement> screen = toolbar.findElements(By.className("android.widget.TextView"));
+        sleep.click();
+        Assert.assertEquals( "Sleep", screen.get(0).getText());
     }
 
     @Test
-    /* TC068 */
-    public void showsButtons() {
+    /* TC067 */
+    public void correctScreenIot() throws InterruptedException {
+        menuScreen();
+        MobileElement iot = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/navigation_iot");
+        MobileElement toolbar = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/toolbar");
+        List<MobileElement> screen = toolbar.findElements(By.className("android.widget.TextView"));
+        iot.click();
+        Assert.assertEquals( "IoT", screen.get(0).getText());
+    }
 
+    @Test
+    /* TC067 */
+    public void correctScreenPhysicalActivities() throws InterruptedException {
+        menuScreen();
+        MobileElement activities = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/navigation_activities");
+        MobileElement toolbar = (MobileElement) driver.findElementById("br.edu.uepb.nutes.ocariot:id/toolbar");
+        List<MobileElement> screen = toolbar.findElements(By.className("android.widget.TextView"));
+        activities.click();
+        Assert.assertEquals( "Physical activities", screen.get(0).getText());
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
     }
 }
